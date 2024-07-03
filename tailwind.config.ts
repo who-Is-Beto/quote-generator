63470b495c7ac2789459a2a92ca59d555ac169b9
WHOISBEETO <whoisbeto@gmail.com>
2024-07-03 04:35:29 -0600

    [add] app background animation 🌀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,32 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      animation: {
+        gradient: "gradient 6s ease infinite"
+      },
+      keyframes: {
+        gradient: {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "0% 50%"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "100% 50%"
+          }
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
